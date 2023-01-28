6f7ea9c9c10f982c40a3b7992e31c9f02da5e59d
Mark Joseph Cabrera <mjcabrera1996@gmail.com>
2023-01-29 01:10:11 +0800

    Create temporrary solution for copying text in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,16 @@
-const design_request = document.getElementsByClassName("pageDescription").textContent;
+const design_request = document.getElementByClassName("pageDescription").textContent;
 const opportunity = document.getElementById("CF00N0d000002cArw_ileinner").textContent;
 
-navigator.clipboard.writeText(opportunity);
-navigator.clipboard.writeText(design_request);
+// navigator.clipboard.writeText(`${design_request}`);
+// navigator.clipboard.writeText(`${opportunity}`);
 
+setTimeout(() => {navigator.clipboard.writeText(`${design_request}`);}, 2000);
+setTimeout(() => {navigator.clipboard.writeText(`${opportunity}`);}, 1000);
+
+// async () => {
+//     await navigator.clipboard.writeText(`${design_request}`);
+//     await navigator.clipboard.writeText(`${opportunity}`);
+// }
 
 // const designer = "Mark Joseph Cabrera";
 
