a24e736b50c815767567b389544b3234a5218c38
Mark Joseph Cabrera <mjcabrera1996@gmail.com>
2023-01-28 12:53:29 +0800

    Create basic structure for chrome extension

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chrome-extension-sm-productivity-tool
+This is a chrome extension that is use as site modeling productivity tool.
---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+const design_request = document.getElementsByClassName("pageDescription").textContent;
+const opportunity = document.getElementById("CF00N0d000002cArw_ileinner").textContent;
+
+navigator.clipboard.writeText(opportunity);
+navigator.clipboard.writeText(design_request);
+
+
+// const designer = "Mark Joseph Cabrera";
+
+// document.querySelector("#aDE6Q000008OuSL_RelatedEntityHistoryList_body > table > tbody > tr.dataRow.even.first > td:nth-child(2) > a")
